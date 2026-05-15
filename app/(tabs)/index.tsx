7c0f0b329fdfab1d3133995bd47338944e4739af
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-05-15 11:30:02 +0200

    deleted default files

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function TabOneScreen() {
-  return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-green-500 font-bold'>Tab One</Text>
-    </View>
-  );
-}
